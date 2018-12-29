9460bc0e7706fdf6524b092c8fce75e439488b51
sravyamamidishetty <sravyamamidishetty@gmail.com>
2018-12-29 17:04:24 +0530

    added commennts

--- src/main/java/com/xyz/controllers/AdminController.java
+++ src/main/java/com/xyz/controllers/AdminController.java
@@ -1,7 +1,7 @@
 package com.xyz.controllers;
 
 import java.util.List;
-
+//ksajhd
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
